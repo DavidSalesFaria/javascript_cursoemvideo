9f692f5ef38ef74ee7224a5313be587e686fa9a2
David Shelton <david.faria012@gmail.com>
2023-08-08 19:01:39 -0300

    Uptate repositório

--- Modulos/moduloD/Exercícios Propostos/exercicio4/script.js
+++ Modulos/moduloD/Exercícios Propostos/exercicio4/script.js
@@ -1,15 +1,24 @@
-
 function gerar_tabuada(){
     
-    resp_box = document.querySelector("div.scroll-box")
+    selec_box = document.querySelector("select#seltabu")
     input_num = document.querySelector("input#txtnumero")
     numero = Number(input_num.value)
 
-    for (i=0; i<=10; i++){
+    selec_box.innerHTML = ">"
+    for (i=1; i<=10; i++){
         str_tabuada = `${numero} x ${i} = ${numero * i}`
-        parag = document.createElement("p")
-        parag.setAttribute("class", "linha_tabu")
-        parag.innerHTML = str_tabuada
-        resp_box.appendChild(parag)
+        opt = document.createElement("option")
+        opt.setAttribute("id", `linhatabu${i}`)
+        opt.setAttribute("value", `linhatabu${i}`)
+        opt.setAttribute("onclick", `gerar_resposta()`)
+        opt.innerHTML = str_tabuada
+        selec_box.appendChild(opt)
     }
+}
+
+function gerar_resposta(){
+    selec_box = document.querySelector("select#seltabu")
+    side_box = document.querySelector("aside.sidebox")
+    opt_selecionada = document.querySelector(`option#${selec_box.value}`)
+    side_box.innerHTML = `<h1>${opt_selecionada.innerHTML}</h1>`
 }
