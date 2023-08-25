21abb758cdfa917cd0d6f128183ed0ec83dba2b8
David Shelton <david.faria012@gmail.com>
2023-08-25 16:41:21 -0300

    Update repositorio

--- Modulos/moduloD/Exercícios Propostos/exercicio5/script.js
+++ Modulos/moduloD/Exercícios Propostos/exercicio5/script.js
@@ -1,73 +1,82 @@
-function carregar(){
-    lista_num = []
-    // Selecionar elementos html
-    //
-    // Input
-    input_num = document.querySelector("input#txtnumero");
-    selec_box = document.querySelector("select#selnum");
-    // Resposta
-    resp = document.querySelector("div#resp");
-}
+let lista_num = [];
+const input_num = document.querySelector("input#txtnumero")
+const selec_box = document.querySelector("select#selnum");
+const addButt = document.querySelector("input#addbutt");
+addButt.addEventListener("click", function () {
+  adicionarNum(lista_num);
+});
+const finButt = document.querySelector("input#finbutt");
+finButt.addEventListener("click", function () {
+  finalizar(lista_num);
+});
+const resp = document.querySelector("div#resp");
 
-function adicionarNum(){
-    
-    val = Number(input_num.value)
-    opt = document.createElement("option")
-    opt.setAttribute("id", "opt")
-    opt.innerText = `Valor ${val} adicionado`
-    selec_box.appendChild(opt)
-    lista_num.push(val)
+
+function adicionarNum(lista) {
+  input_num.setAttribute("class", "")
+  val = Number(input_num.value);
+
+  if ((val < 1 || val > 100)) {
+    window.alert("ERRO! Digite um número entre 1 e 100");
     input_num.value = "";
+    input_num.setAttribute("class", "erro")
+  } 
+  else if (lista.includes(val)) {
+    window.alert("ERRO! Número já cadastrado");
+    input_num.setAttribute("class", "erro");
+  } 
+  else {
+    opt = document.createElement("option");
+    opt.setAttribute("id", "opt");
+    opt.innerText = `Valor ${val} adicionado`;
+    selec_box.appendChild(opt);
+    lista.push(val);
+    input_num.value = "";
+    input_num.focus();
+  }
+  
 }
 
-function finalizar(){
+function finalizar(lista) {
+  input_num.setAttribute("class", "");
+  if (selec_box.querySelectorAll("option").length == 0) {
+    window.alert("Insira valores antes de finalizar!");
+    input_num.setAttribute("class", "erro");
+  } else {
 
-    // Quantidade de valores
-    quant = 0
-    // Maior valor
-    maior = 0
-    // Menor valor
-    menor = 0
-    // Somatório
-    soma = 0
-    // Média
-    media = 0
-
+    let quant = 0
     // Quantifica os valores cadastrados
-    for (i=0; i<lista_num.length; i++){
-        quant++
+    for (i = 0; i < lista.length; i++) {
+      quant++;
     }
 
-    // Define o maior valor
-    for (i=0; i<lista_num.length; i++){
-        if (lista_num[i] > maior)
-            maior = lista_num[i]
+    let maior = lista[0]
+    let menor = lista[0]
+    // Define o maior e o menor valor
+    for (i = 0; i < lista.length; i++) {
+      if (lista[i] > maior) {
+        maior = lista[i];
+      }
+      if (lista[i] < menor){
+        menor = lista[i]
+      }
     }
 
-    // Define o menor valor
-    for (i=0; i<lista_num.length; i++){
-        if (i == 0)
-            menor = lista_num[i]
-        else (lista_num[i] < menor)
-            menor = lista_num[i]
-    }
-
+    let soma = 0
     // Calcula a soma dos valores cadastrados
-    for (i=0; i<lista_num.length; i++){
-        soma += lista_num[i]
+    for (i = 0; i < lista.length; i++) {
+      soma += lista[i];
     }
 
     // Calcula a média dos valores cadastrados
-    media = soma/quant
+    media = soma / quant;
 
-    // Resposta
-    str_resp = `
-    <p> Ao todo temos ${quant} números cadastrados </p>
-    <p> O maior valor informado foi ${maior} </p>
-    <p> O menor valor informado foi ${menor} </p>
-    <p> A soma de todos os valores é ${soma} </p>
-    <p> A média dos valores é ${media} </p>
-    `;
-
+    str_resp = ``
+    str_resp += `<p> Ao todo temos ${quant} números cadastrados </p>`;
+    str_resp += `<p> O maior valor informado foi ${maior} </p>`;
+    str_resp += `<p> O menor valor informado foi ${menor} </p>`;
+    str_resp += `<p> A soma de todos os valores é ${soma} </p>`;
+    str_resp += `<p> A média dos valores é ${media} </p>`;
     resp.innerHTML = str_resp
+  }
 }
